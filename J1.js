992de23823898e5b1dc5a22bb35be08fc60702a1
kdz99 <77607588+kdz99@users.noreply.github.com>
2021-12-19 11:36:18 +0300

    Update J1.js

--- J1.js
+++ J1.js
@@ -4,15 +4,12 @@
 
 <script>
   
-    
-   
-    
+ 
 function tick() {
   const element = (
     <div>
     
-      
-      
+    
       <h1>Hello, world!</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
